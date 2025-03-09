7a0caccf05076a963a20102fa83c143ae6fe79a0
Tan Nguyen <nt7nguye@uwaterloo.ca>
2025-03-09 17:13:27 -0400

    chore: fix the imports again, i might be stupid

--- blackjack/api/core/__init__.py
+++ blackjack/api/core/__init__.py
@@ -2,7 +2,7 @@ from core.card import Card, Suit
 from core.deck import Deck
 from core.game import BlackjackGame, GameState, Action, PlayerInformation
 from core.hand import Hand
-from blackjack.api.core.strategy import Strategy
+from core.strategy import Strategy
 from core.simulator import BlackjackSimulator
 
 __all__ = [
